64df0b3dce2bcf42491587c5447c8c97bdd7fafd
Thorben Heins <mail@thorben-heins.de>
2014-03-20 15:06:42 +0100

    cosmetics

--- js/translations.js
+++ js/translations.js
@@ -1,16 +1,41 @@
 var translationSet = {
 	//HTML Translations
-	"TRmessaging": "Standby while we prepare a free preview of the latest issue.", 
+	
+	// Standby while we prepare a free preview of the latest issue.
+	"TRmessaging": "Standby while we prepare a free preview of the latest issue.",
+	
+	// I would like to subscribe
 	"TRsubscribe-button": "I would like to subscribe",
+	
+	// I am a print subscriber
 	"TRprint-subscriber-button": "I am a print subscriber",
+	
+	// I am a digital subscriber
 	"TRitunes-button": "I am a digital subscriber",
+	
+	// Go to store
 	"TRstore-button": "Go to store",
+	
 	//Translations in the JavaScript
+	
+	// Please connect to the internet
 	"TRplease-connect-to-the-internet": "Please connect to the internet.",
+	
+	// Go to library
 	"TRgo-to-library": "Go to library",
+	
+	//Select a digital subscription option below. Your digital subscription will start immediately from the latest issue after you complete the purchase process.
 	"TRsubscribe-button-title": "Select a digital subscription option below. Your digital subscription will start immediately from the latest issue after you complete the purchase process.",
+	
+	// subscription for
 	"TRsubsription-for": "subscription for",
+	
+	// Unable to purchase subscription.
 	"TRunable-to-purchase-subscription": "Unable to purchase subscription.",
+	
+	// Enjoy your free preview of
 	"TRenjoy-your-free-preview": "Enjoy your free preview of",
+	
+	// View issue
 	"TRview-issue": "View issue"
 }
